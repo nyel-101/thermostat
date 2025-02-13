707f94735984c1de5382633ddb436b18f608154e
nyel-101 <dandevjr@gmail.com>
2025-02-13 02:06:41 +0000

    style(auth): restructure middleware routes

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c51dac9dae2043aebba2d02ef3be7872 = () => {
-  return '2026-04-21T20:29:00Z';
+const node_8544e6b7d77a425aad235eb7d243db5c = () => {
+  return '2025-02-13T02:06:41Z';
 };
