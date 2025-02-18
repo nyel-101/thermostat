5e0d028a0f076662e2b3a456f036b22299b2a36c
nyel-101 <dandevjr@gmail.com>
2025-02-18 15:51:09 +0000

    fix(config): restructure session store

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fbcfab23e20494e9da34db94bdb898f = () => {
-  return '2026-04-20T21:47:12Z';
+const node_661e32922b2c4079998ff09ea5f0d469 = () => {
+  return '2025-02-18T15:51:09Z';
 };
