ed7d3dc77f4015a1d163b907339b2892090a8053
nyel-101 <dandevjr@gmail.com>
2025-02-20 13:31:46 +0000

    chore(deps): refactor payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fcdc5ab2905041899edc2bc4f3b96f53 = () => {
-  return '2026-02-06T08:17:38Z';
+const node_cb8d9221764443e58bc2dd5644b9b7d0 = () => {
+  return '2025-02-20T13:31:46Z';
 };
