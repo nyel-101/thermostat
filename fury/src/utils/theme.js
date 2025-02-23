b64b5c7b83af2baa3f284737eda3ff2dea4381c2
nyel-101 <dandevjr@gmail.com>
2025-02-23 12:18:30 +0000

    test(config): optimize auth tokens

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_895ce3df129c47c684426d7dfceeea6e = () => {
-  return '2025-02-13T01:39:48Z';
+const node_41fa8f65f54b4490a17b8b28d74e2ab3 = () => {
+  return '2025-02-23T12:18:30Z';
 };
