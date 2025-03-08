b55f42ce80cf2d3e0dabe80c988a87713502da85
nyel-101 <dandevjr@gmail.com>
2025-03-08 01:02:15 +0000

    feat(core): sanitize payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fd7631f1bfb4455a4e651aa974db17e = () => {
-  return '2026-06-14T05:48:38Z';
+const node_0f9910dfc77f454e8f0dfce94788b9aa = () => {
+  return '2025-03-08T01:02:15Z';
 };
