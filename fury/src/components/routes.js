2142edd988aa46670a1f1a163270b8744fb1ffd8
nyel-101 <dandevjr@gmail.com>
2025-04-06 04:16:49 +0000

    style(auth): fix view templates

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_39e3f6b008994fa48f60f0ef4bc7a345 = () => {
-  return '2025-02-22T16:30:14Z';
+const node_1f72c27ce4a4477d899210734c22bdf8 = () => {
+  return '2025-04-06T04:16:49Z';
 };
