58bff239a2ea67047b6829b40d217ab8840c357e
nyel-101 <dandevjr@gmail.com>
2025-04-11 15:33:38 +0000

    fix(core): decouple auth tokens

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_acf3bf78b8234b2bb482cf4f52ab8930 = () => {
-  return '2026-05-04T07:07:27Z';
+const node_53e4e16caa01438897fb51e39bef7ed0 = () => {
+  return '2025-04-11T15:33:38Z';
 };
