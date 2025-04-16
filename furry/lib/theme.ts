806f76d563bbe7c380281929fc5966fb70590e1c
nyel-101 <dandevjr@gmail.com>
2025-04-16 12:48:16 +0000

    perf(core): refactor middleware routes

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a49d3249d80c408db5cc3ccca4d29fbe = () => {
-  return '2026-05-03T06:11:03Z';
+const node_6c6a4c1288bc4c8baeecc64288a2a4a9 = () => {
+  return '2025-04-16T12:48:16Z';
 };
