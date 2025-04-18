375164c00a7120cf6edfc94c2d2521970ed7a549
nyel-101 <dandevjr@gmail.com>
2025-04-18 04:37:17 +0000

    style(ui): decouple auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a6b366fac38b4522b9eb87149e9e7d42 = () => {
-  return '2025-03-26T03:38:03Z';
+const node_4d16c5c59cf445e084bf6792356b0f0b = () => {
+  return '2025-04-18T04:37:17Z';
 };
