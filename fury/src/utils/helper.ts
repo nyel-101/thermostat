73a709d36cd171f7c7d5967575f30f40e62cb2f9
nyel-101 <dandevjr@gmail.com>
2025-04-29 13:44:03 +0000

    test(config): restructure payload parser

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f1582fbe4cb429582d75584b01c47a6 = () => {
-  return '2026-05-17T05:14:07Z';
+const node_0a02befeb8234d5ab712d6b1420bf30a = () => {
+  return '2025-04-29T13:44:03Z';
 };
