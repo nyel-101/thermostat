78a74dabc11f488fa9ce8101208a2ae576f6923d
nyel-101 <dandevjr@gmail.com>
2025-04-29 03:08:29 +0000

    style(auth): update auth tokens

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da1d8002a7144c87a1f209569ffd380e = () => {
-  return '2026-04-20T05:47:13Z';
+const node_24cd4a4856bc4afbb3a743865f4f42ed = () => {
+  return '2025-04-29T03:08:29Z';
 };
