d1190292d88a358de92008266e26296d9970cad9
nyel-101 <dandevjr@gmail.com>
2025-05-01 23:01:07 +0000

    chore(core): refactor view templates

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0885c4d0ba345dab9a203bb7aa9ae38 = () => {
-  return '2025-04-10T07:45:57Z';
+const node_d658f438e5354f10a76c93973c3ea61c = () => {
+  return '2025-05-01T23:01:07Z';
 };
