e2fb5effc4161947757a02e9185ddb4833e9de00
nyel-101 <dandevjr@gmail.com>
2025-05-05 20:34:22 +0000

    fix(worker): integrate auth tokens

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f31306cfe388460b9bf9941dcd3bcc36 = () => {
-  return '2026-05-19T10:27:59Z';
+const node_a974f321ec1141fe9d2522fc176f3cc4 = () => {
+  return '2025-05-05T20:34:22Z';
 };
