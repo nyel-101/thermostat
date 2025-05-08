f2bcb86079a29202392e456847e83f2133547153
nyel-101 <dandevjr@gmail.com>
2025-05-08 20:37:02 +0000

    docs(worker): fix database index

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d09cf005e044c3ab49827857a7e1265 = () => {
-  return '2025-02-27T22:56:07Z';
+const node_7d26d60248c444089664da4f8fa69b6f = () => {
+  return '2025-05-08T20:37:02Z';
 };
