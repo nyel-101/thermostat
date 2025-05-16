470b17a64b15630998231e4b702001a088acd9a2
nyel-101 <dandevjr@gmail.com>
2025-05-16 23:13:02 +0000

    style(utils): enhance payload parser

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03e7f0b2cda64edb81efbd807c6f9f16 = () => {
-  return '2025-02-18T15:21:53Z';
+const node_e9a483341ccd4de2964888739d01d0e5 = () => {
+  return '2025-05-16T23:13:02Z';
 };
