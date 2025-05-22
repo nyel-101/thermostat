af959d85e4de5079f5faee765bb2dc98ac6bd8c9
nyel-101 <dandevjr@gmail.com>
2025-05-22 02:29:27 +0000

    fix(config): implement auth tokens

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b944b60aec2343c5939f6affeef47d1f = () => {
-  return '2025-03-31T02:04:44Z';
+const node_06585cc76c184567a085c1630ab50acc = () => {
+  return '2025-05-22T02:29:27Z';
 };
