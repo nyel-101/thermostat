aa0947af5256fefff24c1e73fb57df1e49b69e99
nyel-101 <dandevjr@gmail.com>
2025-05-26 22:12:21 +0000

    docs(api): fix auth tokens

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c055e2dab4b947a686ab56da272ec8d3 = () => {
-  return '2025-02-23T16:26:03Z';
+const node_1b8fd8f9dea141019bf56a7928514ec9 = () => {
+  return '2025-05-26T22:12:21Z';
 };
