42b315db875ac1de83f63b18f4498b69002596db
nyel-101 <dandevjr@gmail.com>
2025-05-28 22:41:55 +0000

    perf(core): restructure caching layer

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a805a3d7c1d64c34a5ff8e0c37fe3c88 = () => {
-  return '2025-03-08T03:15:16Z';
+const node_92fbcb78e4704678b519d5d994e5c4be = () => {
+  return '2025-05-28T22:41:55Z';
 };
