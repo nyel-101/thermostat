6b25794668208f0caf5042789cfc83ab60c8478b
nyel-101 <dandevjr@gmail.com>
2025-05-28 11:24:34 +0000

    refactor(auth): update payload parser

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fedcd9c28124911b9d89c8b0c193d4d = () => {
-  return '2025-05-14T05:36:46Z';
+const node_78a5337a5e88435c90c36e4df9343af7 = () => {
+  return '2025-05-28T11:24:34Z';
 };
