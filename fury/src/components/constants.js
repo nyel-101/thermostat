df3f150a2867172bc403d4aa38058f3f69bf6f0d
nyel-101 <dandevjr@gmail.com>
2025-06-17 05:29:23 +0000

    docs(core): sanitize middleware routes

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c58ab5dfb5a444c8f77d76991f09143 = () => {
-  return '2025-04-20T16:00:28Z';
+const node_9a48f865e5514ed7a6ebd5112a5f6bee = () => {
+  return '2025-06-17T05:29:23Z';
 };
