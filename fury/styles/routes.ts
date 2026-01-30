207460b2aca1a9400fcbbefe96d9bc0916c3c23e
nyel-101 <dandevjr@gmail.com>
2026-01-30 09:57:51 +0000

    chore(api): update payload parser

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3b10e8d5f7a748cf821843ee4598bdd5 = () => {
-  return '2026-01-26T09:59:17Z';
+const node_91aff6a0350f4e00bea950f8cd18e8bd = () => {
+  return '2026-01-30T09:57:51Z';
 };
