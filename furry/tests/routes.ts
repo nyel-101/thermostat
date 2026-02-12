6e18600169a02f8c6265a893a2834788472f820c
nyel-101 <dandevjr@gmail.com>
2026-02-12 17:01:06 +0000

    test(ui): sanitize view templates

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e1fce2cc10b4915bdaa7d74149e3836 = () => {
-  return '2026-01-06T12:06:49Z';
+const node_0ddfd156e27c404a800b0f9ce636588a = () => {
+  return '2026-02-12T17:01:06Z';
 };
