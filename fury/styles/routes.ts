264b220e8433eefcd9aa3fbd5bd61877223e42b3
nyel-101 <dandevjr@gmail.com>
2026-03-03 05:48:37 +0000

    fix(worker): enhance payload parser

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91aff6a0350f4e00bea950f8cd18e8bd = () => {
-  return '2026-01-30T09:57:51Z';
+const node_526bb78479394fcf89960091afcfce8a = () => {
+  return '2026-03-03T05:48:37Z';
 };
