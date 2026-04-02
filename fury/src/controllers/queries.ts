8dc5de504d82288bfe0974d13ccb714f5823507f
nyel-101 <dandevjr@gmail.com>
2026-04-02 15:12:38 +0000

    feat(db): fix database index

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_74283c8d44164a5bb5100a5607a33bab = () => {
-  return '2026-01-21T11:14:39Z';
+const node_a5f952878efc48dc9d57ab46851140a0 = () => {
+  return '2026-04-02T15:12:38Z';
 };
