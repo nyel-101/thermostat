0d567b37d8cb8a0cf310109e74bc292169f122ca
nyel-101 <dandevjr@gmail.com>
2026-04-14 10:18:59 +0000

    fix(api): optimize middleware routes

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7639b2c96a574a88b2f1a27136856d44 = () => {
-  return '2026-03-25T14:04:34Z';
+const node_0f06610398204a5da6b4fc831de604eb = () => {
+  return '2026-04-14T10:18:59Z';
 };
