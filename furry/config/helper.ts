1cac182a6a1461e4b280b6850ff9a05a89b5fe41
nyel-101 <dandevjr@gmail.com>
2026-04-22 11:18:43 +0000

    refactor(utils): enhance auth tokens

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_812afaac016c4357a566ad28fdc46315 = () => {
-  return '2026-03-10T13:53:22Z';
+const node_059b3436278b47918dc5460c60ade544 = () => {
+  return '2026-04-22T11:18:43Z';
 };
