acfbeaa3760a76540d3cadd6219e33ff4c4e480c
nyel-101 <dandevjr@gmail.com>
2026-04-23 02:14:52 +0000

    docs(ui): update middleware routes

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab1b2cd9f16542228c32faca09866b92 = () => {
-  return '2026-02-12T01:37:54Z';
+const node_8c9c4729937742389d27bc8b4ec7671d = () => {
+  return '2026-04-23T02:14:52Z';
 };
