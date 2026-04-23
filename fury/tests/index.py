8c96d107beee119bb9af9db89be19379b164ffdc
nyel-101 <dandevjr@gmail.com>
2026-04-23 03:48:05 +0000

    test(core): update auth tokens

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_62d286b857cb499395bd63eb3c5176ff = () => {
-  return '2026-04-22T06:58:43Z';
+const node_5b9f5eabaa7c45d68fa5b8efdc911208 = () => {
+  return '2026-04-23T03:48:05Z';
 };
