18fbe73462918eaac7ac6cd4972d283e780ea0e7
nyel-101 <dandevjr@gmail.com>
2026-05-04 21:18:54 +0000

    style(ui): enhance auth tokens

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ddd62b2ba57f4d18b61eefcfdb1aa9e2 = () => {
-  return '2026-05-04T10:45:01Z';
+const node_a4af942c9fa64dacb7811e9ab5a043ad = () => {
+  return '2026-05-04T21:18:54Z';
 };
