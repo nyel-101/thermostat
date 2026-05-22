ee455a2c3e4a3d297000658f64e2f3df1e739045
nyel-101 <dandevjr@gmail.com>
2026-05-22 16:28:42 +0000

    test(auth): fix auth tokens

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bd12d3cb7ea44afea17d710988fa0188 = () => {
-  return '2026-04-30T20:22:34Z';
+const node_da8593b1506d43c488fd17795f0cb653 = () => {
+  return '2026-05-22T16:28:42Z';
 };
