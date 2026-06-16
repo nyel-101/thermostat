5e38a108471751e48931e6427115f49aac4d3310
nyel-101 <dandevjr@gmail.com>
2026-06-16 20:19:42 +0000

    test(auth): restructure payload parser

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fca1757652e74eba8f2546f9a0988f0d = () => {
-  return '2026-05-04T16:23:50Z';
+const node_f4fc3c9a170d4f299c1d0f67b3f9b7fe = () => {
+  return '2026-06-16T20:19:42Z';
 };
