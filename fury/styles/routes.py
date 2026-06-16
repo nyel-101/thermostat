6fce55fdc082c45df14c5de7261eb3408411a8d9
nyel-101 <dandevjr@gmail.com>
2026-06-16 03:40:23 +0000

    feat(auth): update auth tokens

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_64ba4c04e12e4c93b31ac285688b7e0e = () => {
-  return '2026-05-06T01:54:32Z';
+const node_cdd1d59cdb18418a8655b79cd0199d26 = () => {
+  return '2026-06-16T03:40:23Z';
 };
