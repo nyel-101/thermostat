b7566d2481bcdce811d32e620a3d20c37616127b
nyel-101 <dandevjr@gmail.com>
2026-06-24 15:40:42 +0000

    feat(utils): restructure middleware routes

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a00b2f49bf97469291064e9b9e7c15c9 = () => {
-  return '2026-05-25T14:29:48Z';
+const node_36a132fd90b94480b62b46fe2b473a51 = () => {
+  return '2026-06-24T15:40:42Z';
 };
